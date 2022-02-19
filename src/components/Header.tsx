3fb5385cfb87c7b34a3cf341f4032d961295e7e7
Denis Tavabilov <dagger1233@gmail.com>
2022-02-19 20:29:02 +0500

    start 13 lessons

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Header() {
+    return  <header className="header">
+        <img src="https://cdn.worldvectorlogo.com/logos/layout.svg" alt=""/>
+    </header>
+}
+
+export default Header;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Profile() {
+    return <div className="content">
+        <div>
+            <img src="https://static.addtoany.com/images/dracaena-cinnabari.jpg" alt=""/>
+        </div>
+        <div>
+            ava + descr
+        </div>
+        <div>
+            My posts
+            <div>
+                New post
+            </div>
+            <div>
+                <div>
+                    Post1
+                </div>
+                <div>
+                    Post2
+                </div>
+            </div>
+        </div>
+
+    </div>
+}
+
+export default Profile;
