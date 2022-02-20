55f0843aee6f34d31daa2f4445301a251d551d5a
Denis Tavabilov <dagger1233@gmail.com>
2022-02-20 21:59:00 +0500

    end 17 lessons2

--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -9,6 +9,7 @@ function Post(props:PostPropsType) {
     return <div className={classes.item}>
         <img src="https://www.meme-arsenal.com/memes/8ab5fe07681cd172915e9472a0a8443d.jpg" alt=""/>
         {props.message}
+        {props.message}
         <div>
             <span>Like</span>
         </div>
